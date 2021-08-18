38b9a57b3d4d9dbadfa942c1f6761553414366ee
peachy-1 <55264958+peachy-1@users.noreply.github.com>
2021-08-17 23:00:35 -0400

    Email
    Added the HaveIBeenPwned functionality/QoL

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from password_verifier import password_strength
 from flask_bootstrap import Bootstrap
+import requests
 
 app = Flask(__name__, template_folder='./templates')
 Bootstrap(app)
@@ -9,12 +10,28 @@ Bootstrap(app)
 def index():
     return render_template('index.html')
 
-@app.route('/result', methods = [ 'POST', 'GET' ])
-def result():
+@app.route('/password_result', methods = [ 'POST', 'GET' ])
+def password_result():
     if request.method == 'POST':
         password = request.form['password']
         result = password_strength(password)
-        return render_template('result.html', result = result)
+        return render_template('password_result.html', result = result)
+
+@app.route('/email_result', methods = [ 'POST', 'GET' ])
+def email_result():
+    if request.method == 'POST':
+        hibp_key = {'hibp-api-key': '009274fc7df0484a98f9ff01699097a7'}
+        email_address = request.form['email']
+        response = requests.request('GET', f'https://haveibeenpwned.com/api/v3/breachedaccount/{email_address}', headers = hibp_key)
+
+        if response.status_code !=404:
+            results = response.json()
+            is_compromised = True
+        else:
+            results = []
+            is_compromised = False
+
+        return render_template('email_result.html', results = results, is_compromised = is_compromised)
 
 if __name__ == '__main__':
     app.run(debug=True)
